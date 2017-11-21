d45e8e1937b9e4d8cfe104086ab223f35933e8e2
Maciek <maciejpietranik@gmail.com>
2017-11-21 17:06:50 +0100

    Added password authentification and reset password by email

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -107,3 +107,11 @@ from django.core.urlresolvers import reverse_lazy
 LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
 LOGIN_URL = reverse_lazy('login')
 LOGOUT_URL = reverse_lazy('logout')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
