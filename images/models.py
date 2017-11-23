84b77cd283ffb95bfe6285bf92d449d45fa59eea
Maciek <maciejpietranik@gmail.com>
2017-11-23 13:16:11 +0100

    Added signals for denormalizing likes counter

--- images/models.py
+++ images/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.utils.text import slugify
 from django.core.urlresolvers import reverse
 
+
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              related_name='images_created')
@@ -16,6 +17,7 @@ class Image(models.Model):
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                         related_name='images_liked',
                                         blank=True)
+    total_likes = models.PositiveIntegerField(db_index=True, default=0)
 
     def __str__(self):
         return self.title
@@ -24,5 +26,6 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super(Image, self).save(*args, **kwargs)
+
     def get_absolute_url(self):
         return reverse('images:detail', args=[self.id, self.slug])
